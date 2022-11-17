d6c396bd5a2bd513fbacaa9b4ab9694638e91b56
Shubhad Gonal <shubhadgonal19@gmail.com>
2022-11-17 10:04:27 +0530

    Fibonacci series

---
+++ Day6LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6LogicalPrograms
+{
+    internal class FibonacciSeries
+    {
+        public static void fibonacciSeries(int n)
+        {
+           int  firstNum=0,secondNum=1,sum=0;
+            Console.WriteLine("{0},{1}\n",firstNum,secondNum);
+            for(int i = 0; sum <n; i++)
+            {
+                sum=firstNum+secondNum;
+                firstNum = secondNum;
+                secondNum = sum;
+                Console.Write(sum+",");
+            }
+        }
+    }
+}
--- Day6LogicalPrograms/Program.cs
+++ Day6LogicalPrograms/Program.cs
@@ -11,6 +11,16 @@ namespace Day6LogicalPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Logical Programs");
+            Console.WriteLine("enter the option\n1. Fibonacci Series");
+            int option=Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    Console.WriteLine("enter a number");
+                    int n=Convert.ToInt32(Console.ReadLine());
+                    FibonacciSeries.fibonacciSeries(n);
+                    break;
+            }
             Console.ReadLine();
         }
     }
